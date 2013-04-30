d3dc1af0316a58c884468855c862cdc72ebd115a
Richard Herrera <rich@doctyper.com>
2013-04-30 11:35:56 -0700

    make sure dev file exists.

--- test/runner.js
+++ test/runner.js
@@ -1,17 +1,43 @@
-var Mocha = require("mocha");
+var checkForDevFile = function (callback) {
+	var cwd = process.cwd();
+	var fs = require("fs");
+	var path = require("path");
 
-var mocha = new Mocha({
-	setup : "bdd",
-	reporter : process.env.TRAVIS ? "tap" : "spec",
-	slow : 5000,
-	timeout : 30000
-});
+	var testFile = path.join(cwd, "lib", "modernizr-dev.js");
+	var remoteTestFile = "http://modernizr.com/downloads/modernizr-latest.js";
 
-mocha.addFile("test/tests.js");
+	if (fs.existsSync(testFile)) {
+		return callback();
+	} else {
+		var http = require("http");
 
-var runner = mocha.run();
+		var file = fs.createWriteStream(testFile);
+		var request = http.get(remoteTestFile, function (response) {
+			response.pipe(file);
 
-runner.on("fail", function (test, err) {
-	process.stderr.write("         " + err.toString() + "\n\n");
-	process.exit(1);
+			response.on("end", function () {
+				callback();
+			});
+		});
+	}
+};
+
+checkForDevFile(function () {
+	var Mocha = require("mocha");
+
+	var mocha = new Mocha({
+		setup : "bdd",
+		reporter : process.env.TRAVIS ? "tap" : "spec",
+		slow : 5000,
+		timeout : 30000
+	});
+
+	mocha.addFile("test/tests.js");
+
+	var runner = mocha.run();
+
+	runner.on("fail", function (test, err) {
+		process.stderr.write("         " + err.toString() + "\n\n");
+		process.exit(1);
+	});
 });
