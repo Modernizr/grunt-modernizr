49456da8101c970e00dccc3ae375b5487ecd7272
Richard Herrera <rich@doctyper.com>
2013-06-05 11:54:48 -0700

    Update tests.

--- test/js/amd.js
+++ test/js/amd.js
@@ -2,59 +2,180 @@
 /* global define */
 
 define(["Modernizr"], function (M) {
+
 	var tests = {
-		"canvastext" : M.canvastext,
-		"csstransforms3d": M.csstransforms3d,
-		"flexbox": M["flexbox"],
-		"cssgradients": M.cssgradients,
-		"opacity": M.opacity,
-		"indexedDB": M.indexedDB,
-		"backgroundsize": M.backgroundsize,
-		"borderimage": M.borderimage,
-		"borderradius": M.borderradius,
-		"boxshadow": M.boxshadow,
-		"cssanimations": M.cssanimations,
-		"csscolumns": M.csscolumns,
-		"cssreflections": M.cssreflections,
-		"csstransitions": M.csstransitions,
-		"testallprops": M.testallprops,
-		"flexbox-legacy": M["flexbox-legacy"],
-		"prefixed": M.prefixed,
-		"csstransforms": M.csstransforms,
-		"mq": M.mq,
-		"hashchange": M.hashchange,
-		"draganddrop": M.draganddrop,
-		"generatedcontent": M.generatedcontent,
-		"svg": M.svg,
-		"inlinesvg": M.inlinesvg,
-		"smil": M.smil,
-		"svgclippaths": M.svgclippaths,
-		"input": M.input,
-		"inputtypes": M.inputtypes,
-		"touch": M.touch,
-		"fontface": M.fontface,
-		"testbundle": M.testbundle,
-		"respond": M.respond,
-		"websockets": M.websockets,
-		"applicationcache": M.applicationcache,
-		"audio": M.audio,
-		"canvas": M.canvas,
-		"geolocation": M.geolocation,
-		"history": M.history,
-		"hsla": M.hsla,
-		"indexeddb": M.indexeddb,
-		"localstorage": M.localstorage,
-		"multiplebgs": M.multiplebgs,
-		"postmessage": M.postmessage,
-		"scriptdefer": M.scriptdefer,
-		"sessionstorage": M.sessionstorage,
-		"textshadow": M["textshadow"],
-		"rgba": M.rgba,
-		"video": M.video,
-		"webgl": M.webgl,
-		"websqldatabase": M.websqldatabase,
-		"webworkers": M.webworkers
+		"standalone": {
+			"audio": M.audio,
+			"touchevents": M.touchevents,
+			"websockets": M.websockets
+		},
+		"a": {
+			"download": M.adownload
+		},
+		"audio": {
+			"audiodata": M.audiodata,
+			"loop": M.audioloop,
+			"preload": M.audiopreload,
+			"webaudio": M.webaudio
+		},
+		"battery": {
+			"lowbattery": M.lowbattery
+		},
+		"canvas": {
+			"todataurl": [
+				M["todataurljpeg"],
+				M["todataurlpng"],
+				M["todataurlwebp"]
+			]
+		},
+		"css": {
+			"animations": M.cssanimations,
+			"backgroundposition-shorthand": M.bgpositionshorthand,
+			"backgroundposition-xy": M.bgpositionxy,
+			"backgroundrepeat": [
+				M["bgrepeatspace"],
+				M["bgrepeatround"]
+			],
+			"backgroundsize": M.backgroundsize,
+			"backgroundsizecover": M.bgsizecover,
+			"borderimage": M.borderimage,
+			"borderradius": M.borderradius,
+			"boxshadow": M.boxshadow,
+			"boxsizing": M.boxsizing,
+			"calc": M.csscalc,
+			"checked": M.checked,
+			"columns": M.csscolumns,
+			"cubicbezierrange": M.cubicbezierrange,
+			"displayrunin": M["display-runin"],
+			"displaytable": M["display-table"],
+			"filters": M.cssfilters,
+			"flexbox": M.flexbox,
+			"flexboxlegacy": M.flexboxlegacy,
+			"fontface": M.fontface,
+			"generatedcontent": M.generatedcontent,
+			"gradients": M.cssgradients,
+			"hsla": M.hsla,
+			"hyphens": [
+				M["csshyphens"],
+				M["softhyphens"],
+				M["softhyphensfind"]
+			],
+			"lastchild": M.lastchild,
+			"mask": M.lastchild,
+			"mediaqueries": M.mediaqueries,
+			"multiplebgs": M.multiplebgs,
+			"objectfit": M.objectfit,
+			"opacity": M.opacity,
+			"overflow-scrolling": M.overflowscrolling,
+			"pointerevents": M.csspointerevents,
+			"positionsticky": M.csspositionsticky,
+			"pseudoanimations": M.csspseudoanimations,
+			"pseudotransitions": M.csspseudotransitions,
+			"reflections": M.cssreflections,
+			"regions": M.regions,
+			"remunit": M.cssremunit,
+			"resize": M.cssresize,
+			"rgba": M.rgba,
+			"scrollbars": M.cssscrollbar,
+			"shapes": M.shapes,
+			"siblinggeneral": M.siblinggeneral,
+			"subpixelfont": M.subpixelfont,
+			"supports": M.supports,
+			"textshadow": M.textshadow,
+			"transforms": M.csstransforms,
+			"transforms3d": M.csstransforms3d,
+			"transitions": M.csstransitions,
+			"userselect": M.userselect,
+			"vhunit": M.cssvhunit,
+			"vmaxunit": M.cssvmaxunit,
+			"vminunit": M.cssvminunit,
+			"vwunit": M.cssvwunit,
+			"wrapflow": M.wrapflow
+		},
+		"dom": {
+			"classlist": M.classlist,
+			"createElement-attrs": [
+				M["createelementattrs"],
+				M["createelement-attrs"]
+			],
+			"dataset": M.dataset,
+			"microdata": M.microdata
+		},
+		"elem": {
+			"datalist": M.datalistelem,
+			"details": M.details,
+			"output": M.outputelem,
+			"progress-meter": [
+				M["progressbar"],
+				M["meter"]
+			],
+			"ruby": M.ruby,
+			"template": M.template,
+			"time": M.time,
+			"track": [
+				M["texttrackapi"],
+				M["track"]
+			]
+		},
+		"es5": {
+			"array": M.es5array,
+			"date": M.es5date,
+			"function": M.es5function,
+			"object": M.es5object,
+			"strictmode": M.strictmode,
+			"string": M.es5string
+		},
+		"es6": {
+			"contains": M.contains
+		},
+		"event": {
+			"deviceorientation-motion": [
+				M["devicemotion"],
+				M["deviceorientation"]
+			]
+		},
+		"file": {
+			"api": M.filereader,
+			"filesystem": M.filesystem
+		},
+		"forms": {
+			"capture": M.capture,
+			"fileinput": M.fileinput,
+			"formattribute": M.formattribute,
+			"inputnumber-l10n": M.localizednumber,
+			"placeholder": M.placeholder,
+			"requestautocomplete": M.requestautocomplete,
+			"speechinput": M.speechinput,
+			"validation": M.formvalidation
+		},
+		"iframe": {
+			"sandbox": M.sandbox,
+			"seamless": M.seamless,
+			"srcdoc": M.srcdoc
+		},
+		"img": {
+			"apng": M.apng,
+			"webp-lossless": [
+				M["webplossless"],
+				M["webp-lossless"]
+			],
+			"webp": M.webp
+		},
+		"network": {
+			"connection": M.lowbandwidth,
+			"eventsource": M.eventsource,
+			"xhr2": M.xhr2
+		},
+		"svg": {
+			"asimg": M.svgasimg
+		},
+		"video": {
+			"autoplay": M.videoautoplay,
+			"loop": M.videoloop,
+			"preload": M.videopreload
+		}
 	};
 
 	return M;
+
 });
--- test/js/vanilla.js
+++ test/js/vanilla.js
@@ -3,55 +3,174 @@
 var Modernizr = window.Modernizr;
 
 var tests = {
-	"canvastext" : Modernizr.canvastext,
-	"csstransforms3d": Modernizr.csstransforms3d,
-	"flexbox": Modernizr["flexbox"],
-	"cssgradients": Modernizr.cssgradients,
-	"opacity": Modernizr.opacity,
-	"indexedDB": Modernizr.indexedDB,
-	"backgroundsize": Modernizr.backgroundsize,
-	"borderimage": Modernizr.borderimage,
-	"borderradius": Modernizr.borderradius,
-	"boxshadow": Modernizr.boxshadow,
-	"cssanimations": Modernizr.cssanimations,
-	"csscolumns": Modernizr.csscolumns,
-	"cssreflections": Modernizr.cssreflections,
-	"csstransitions": Modernizr.csstransitions,
-	"testallprops": Modernizr.testallprops,
-	"flexbox-legacy": Modernizr["flexbox-legacy"],
-	"prefixed": Modernizr.prefixed,
-	"csstransforms": Modernizr.csstransforms,
-	"mq": Modernizr.mq,
-	"hashchange": Modernizr.hashchange,
-	"draganddrop": Modernizr.draganddrop,
-	"generatedcontent": Modernizr.generatedcontent,
-	"svg": Modernizr.svg,
-	"inlinesvg": Modernizr.inlinesvg,
-	"smil": Modernizr.smil,
-	"svgclippaths": Modernizr.svgclippaths,
-	"input": Modernizr.input,
-	"inputtypes": Modernizr.inputtypes,
-	"touch": Modernizr.touch,
-	"fontface": Modernizr.fontface,
-	"testbundle": Modernizr.testbundle,
-	"respond": Modernizr.respond,
-	"websockets": Modernizr.websockets,
-	"applicationcache": Modernizr.applicationcache,
-	"audio": Modernizr.audio,
-	"canvas": Modernizr.canvas,
-	"geolocation": Modernizr.geolocation,
-	"history": Modernizr.history,
-	"hsla": Modernizr.hsla,
-	"indexeddb": Modernizr.indexeddb,
-	"localstorage": Modernizr.localstorage,
-	"multiplebgs": Modernizr.multiplebgs,
-	"postmessage": Modernizr.postmessage,
-	"scriptdefer": Modernizr.scriptdefer,
-	"sessionstorage": Modernizr.sessionstorage,
-	"textshadow": Modernizr["textshadow"],
-	"rgba": Modernizr.rgba,
-	"video": Modernizr.video,
-	"webgl": Modernizr.webgl,
-	"websqldatabase": Modernizr.websqldatabase,
-	"webworkers": Modernizr.webworkers
+	"standalone": {
+		"audio": Modernizr.audio,
+		"touchevents": Modernizr.touchevents,
+		"websockets": Modernizr.websockets
+	},
+	"a": {
+		"download": Modernizr.adownload
+	},
+	"audio": {
+		"audiodata": Modernizr.audiodata,
+		"loop": Modernizr.audioloop,
+		"preload": Modernizr.audiopreload,
+		"webaudio": Modernizr.webaudio
+	},
+	"battery": {
+		"lowbattery": Modernizr.lowbattery
+	},
+	"canvas": {
+		"todataurl": [
+			Modernizr["todataurljpeg"],
+			Modernizr["todataurlpng"],
+			Modernizr["todataurlwebp"]
+		]
+	},
+	"css": {
+		"animations": Modernizr.cssanimations,
+		"backgroundposition-shorthand": Modernizr.bgpositionshorthand,
+		"backgroundposition-xy": Modernizr.bgpositionxy,
+		"backgroundrepeat": [
+			Modernizr["bgrepeatspace"],
+			Modernizr["bgrepeatround"]
+		],
+		"backgroundsize": Modernizr.backgroundsize,
+		"backgroundsizecover": Modernizr.bgsizecover,
+		"borderimage": Modernizr.borderimage,
+		"borderradius": Modernizr.borderradius,
+		"boxshadow": Modernizr.boxshadow,
+		"boxsizing": Modernizr.boxsizing,
+		"calc": Modernizr.csscalc,
+		"checked": Modernizr.checked,
+		"columns": Modernizr.csscolumns,
+		"cubicbezierrange": Modernizr.cubicbezierrange,
+		"displayrunin": Modernizr["display-runin"],
+		"displaytable": Modernizr["display-table"],
+		"filters": Modernizr.cssfilters,
+		"flexbox": Modernizr.flexbox,
+		"flexboxlegacy": Modernizr.flexboxlegacy,
+		"fontface": Modernizr.fontface,
+		"generatedcontent": Modernizr.generatedcontent,
+		"gradients": Modernizr.cssgradients,
+		"hsla": Modernizr.hsla,
+		"hyphens": [
+			Modernizr["csshyphens"],
+			Modernizr["softhyphens"],
+			Modernizr["softhyphensfind"]
+		],
+		"lastchild": Modernizr.lastchild,
+		"mask": Modernizr.lastchild,
+		"mediaqueries": Modernizr.mediaqueries,
+		"multiplebgs": Modernizr.multiplebgs,
+		"objectfit": Modernizr.objectfit,
+		"opacity": Modernizr.opacity,
+		"overflow-scrolling": Modernizr.overflowscrolling,
+		"pointerevents": Modernizr.csspointerevents,
+		"positionsticky": Modernizr.csspositionsticky,
+		"pseudoanimations": Modernizr.csspseudoanimations,
+		"pseudotransitions": Modernizr.csspseudotransitions,
+		"reflections": Modernizr.cssreflections,
+		"regions": Modernizr.regions,
+		"remunit": Modernizr.cssremunit,
+		"resize": Modernizr.cssresize,
+		"rgba": Modernizr.rgba,
+		"scrollbars": Modernizr.cssscrollbar,
+		"shapes": Modernizr.shapes,
+		"siblinggeneral": Modernizr.siblinggeneral,
+		"subpixelfont": Modernizr.subpixelfont,
+		"supports": Modernizr.supports,
+		"textshadow": Modernizr.textshadow,
+		"transforms": Modernizr.csstransforms,
+		"transforms3d": Modernizr.csstransforms3d,
+		"transitions": Modernizr.csstransitions,
+		"userselect": Modernizr.userselect,
+		"vhunit": Modernizr.cssvhunit,
+		"vmaxunit": Modernizr.cssvmaxunit,
+		"vminunit": Modernizr.cssvminunit,
+		"vwunit": Modernizr.cssvwunit,
+		"wrapflow": Modernizr.wrapflow
+	},
+	"dom": {
+		"classlist": Modernizr.classlist,
+		"createElement-attrs": [
+			Modernizr["createelementattrs"],
+			Modernizr["createelement-attrs"]
+		],
+		"dataset": Modernizr.dataset,
+		"microdata": Modernizr.microdata
+	},
+	"elem": {
+		"datalist": Modernizr.datalistelem,
+		"details": Modernizr.details,
+		"output": Modernizr.outputelem,
+		"progress-meter": [
+			Modernizr["progressbar"],
+			Modernizr["meter"]
+		],
+		"ruby": Modernizr.ruby,
+		"template": Modernizr.template,
+		"time": Modernizr.time,
+		"track": [
+			Modernizr["texttrackapi"],
+			Modernizr["track"]
+		]
+	},
+	"es5": {
+		"array": Modernizr.es5array,
+		"date": Modernizr.es5date,
+		"function": Modernizr.es5function,
+		"object": Modernizr.es5object,
+		"strictmode": Modernizr.strictmode,
+		"string": Modernizr.es5string
+	},
+	"es6": {
+		"contains": Modernizr.contains
+	},
+	"event": {
+		"deviceorientation-motion": [
+			Modernizr["devicemotion"],
+			Modernizr["deviceorientation"]
+		]
+	},
+	"file": {
+		"api": Modernizr.filereader,
+		"filesystem": Modernizr.filesystem
+	},
+	"forms": {
+		"capture": Modernizr.capture,
+		"fileinput": Modernizr.fileinput,
+		"formattribute": Modernizr.formattribute,
+		"inputnumber-l10n": Modernizr.localizednumber,
+		"placeholder": Modernizr.placeholder,
+		"requestautocomplete": Modernizr.requestautocomplete,
+		"speechinput": Modernizr.speechinput,
+		"validation": Modernizr.formvalidation
+	},
+	"iframe": {
+		"sandbox": Modernizr.sandbox,
+		"seamless": Modernizr.seamless,
+		"srcdoc": Modernizr.srcdoc
+	},
+	"img": {
+		"apng": Modernizr.apng,
+		"webp-lossless": [
+			Modernizr["webplossless"],
+			Modernizr["webp-lossless"]
+		],
+		"webp": Modernizr.webp
+	},
+	"network": {
+		"connection": Modernizr.lowbandwidth,
+		"eventsource": Modernizr.eventsource,
+		"xhr2": Modernizr.xhr2
+	},
+	"svg": {
+		"asimg": Modernizr.svgasimg
+	},
+	"video": {
+		"autoplay": Modernizr.videoautoplay,
+		"loop": Modernizr.videoloop,
+		"preload": Modernizr.videopreload
+	}
 };
