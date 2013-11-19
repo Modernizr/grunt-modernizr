de8b479eef080e6f5f131cf3126cbdc46dd6fa32
Richard Herrera <rich@doctyper.com>
2013-11-19 12:52:12 -0800

    We're overwriting the default Gruntfile.js when testing different options. This tweak makes sure to reset the file back to the current version in the event of a test failure.

--- test/runner.js
+++ test/runner.js
@@ -12,6 +12,19 @@ mocha.addFile("test/tests.js");
 var runner = mocha.run();
 
 runner.on("fail", function (test, err) {
+	var cp = require("child_process");
+	var cwd = process.cwd();
+	var path = require("path");
+
+	var child = cp.spawn("git", [
+		"checkout", "--", path.join(cwd, "Gruntfile.js")
+	], {
+		stdio: "inherit"
+	});
+
+	child.on("exit", function () {
+		process.exit(1);
+	});
+
 	process.stderr.write("         " + err.toString() + "\n\n");
-	process.exit(1);
 });
