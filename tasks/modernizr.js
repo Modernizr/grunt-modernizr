97f6abdac7de5b258ec879708895fd4d7caa3f8a
Richard Herrera <rich@doctyper.com>
2014-01-13 10:01:50 -0800

    Grunt references are now scoped to this task.

--- tasks/modernizr.js
+++ tasks/modernizr.js
@@ -9,51 +9,11 @@
 module.exports = function (grunt) {
 
 	// ==========================================================================
-	// DEFAULT CONFIG
+	// LOAD SETTINGS
 	// ==========================================================================
 
-	grunt.option("_modernizr.defaults", {
-		// Path to the build you're using for development.
-		"devFile" : false,
-
-		// Path to save out the built file
-		"dest" : "build/modernizr-custom.js",
-
-		// Based on default settings on http://modernizr.com/download/
-		"options" : [
-			"setClasses",
-			"addTest",
-			"html5printshiv",
-			// "load",
-			"testProp",
-			"fnBind"
-		],
-
-		// By default, source is uglified before saving
-		"uglify" : true,
-
-		// Define any tests you want to impliticly include
-		"tests" : [],
-
-		// Useful for excluding any tests that grunt-modernizr will match
-		// e.g. you use .notification class for notification elements,
-		// but don’t want the test for Notification API
-		"excludeTests": [],
-
-		// By default, will crawl your project for references to Modernizr tests
-		// Set to false to disable
-		"crawl" : true,
-
-		// By default, this task will crawl all *.js, *.css, *.scss files.
-		"files" : [
-			"*[^(g|G)runt(file)?].{js,css,scss}",
-			"**[^node_modules]/**/*.{js,css,scss}",
-			"!lib/**/*"
-		],
-
-		// Have custom Modernizr tests? Add them here.
-		"customTests" : []
-	});
+	var path = require("path");
+	var settings = grunt.file.readJSON(path.join(__dirname, "..", "src", "settings.json"));
 
 	// ==========================================================================
 	// TASKS
@@ -67,25 +27,11 @@ module.exports = function (grunt) {
 		// Async
 		var done = this.async();
 
-		// The target from our multi-task
-		var target = this.target || null;
-
 		// The magic
-		var Customizr = require("../src");
+		var Customizr = require(path.join(__dirname, "..", "src"));
 
 		// Go!
-		return new Customizr(grunt, target, done);
-	});
-
-	// ==========================================================================
-	// PRIVATE CONFIG
-	// ==========================================================================
-
-	grunt.option("_modernizr.private", {
-		"url" : {
-			"github" : "https://github.com/doctyper/grunt-modernizr",
-			"domain" : "http://modernizr.com"
-		}
+		return new Customizr(this.data, done);
 	});
 
 };
