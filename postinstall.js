26ae15c4a2096915f25b8dfa50a6be75ccb1f2a0
Thomas Nadin <thomas.nadin@epiphanysolutions.co.uk>
2015-10-15 11:39:18 +0100

    Cross platform support for post install script
    Changed the bash script to a node script to allow support on Windows
    (and non bash environments)

---
+++ postinstall.js
@@ -0,0 +1,23 @@
+// Node Script to perform the original bash command:
+//
+//     if [[ -d node_modules/modernizr ]]; then ( cd node_modules/modernizr; npm install --production; cd -; ); fi
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var on_module_stat = function (error, stat) {
+    if (error === null) {
+        exec('cd node_modules/modernizr && npm install --production', on_install);
+    }
+};
+
+var on_install = function (error, stdout, stderr) {
+    if (error !== null) {
+        console.log('Error executing grunt-modernizr postinstall' + error);
+        console.log(stderr);
+    }
+
+    console.log('grunt-modernizr postinstall sucess');
+};
+
+fs.stat('node_modules/modernizr', on_module_stat);
